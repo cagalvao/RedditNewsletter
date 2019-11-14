b429f45e7097f2273b1c2e638335bc2dde5cddf2
Cássio Soares <cassio.soares@ecolab.com>
2019-11-14 01:02:32 -0300

    fixing email html style

--- src/utils/emailBuilder.js
+++ src/utils/emailBuilder.js
@@ -1,5 +1,5 @@
 export function buildHTML(user, newsletter) {
-  let html = '<html><head><style>body {font-family: Arial Narrow;padding: 10px;width: 900px;}div {display: inline-block;}.header {color: gray;letter-spacing: 2px;font-weight: 600;font-size: 100px;}h1 {color: gray;letter-spacing: 2px;font-weight: 100;}.post {width: 900px;margin-bottom: 30px;}.channel > h1 {display: inline-block;}.channel {border: 3px solid #000;padding: 0 10px;}.img {padding: 5px;display: block}.upvotes {width: 100px;height: 100px;border-radius: 50%;font-size: 36px;font-weight: 600;color: #fff;line-height: 97px;text-align: center;background: orange;}.title {color: #000;font-weight: 500;font-size: 25px;width: 80%;margin-left: 10px;}</style></head><body>';
+  let html = '<html><head><style>body {font-family: Arial Narrow;padding: 10px;width: 900px;}div {display: inline-block;}.header {color: gray;letter-spacing: 2px;font-weight: 600;font-size: 100px;}h1 {color: gray;letter-spacing: 2px;font-weight: 100;}.post {width: 900px;margin-bottom: 30px;}.channel > h1 {display: inline-block;}.channel {border: 3px solid #000;padding: 0 10px;width: 100%;}.img {padding: 5px;display: block;text-align:center;}.img > img {width: 100%}.description {width: 100%;}.upvotes {width: 100px;height: 100px;border-radius: 50%;font-size: 32px;font-weight: 600;color: #fff;line-height: 97px;text-align: center;background: orange;}.title {color: #000;font-weight: 500;font-size: 25px;width: 80%;margin-left: 10px;}</style></head><body>';
   html += `<span class="header">Reddit Newsletter</span><h1>Hello ${user.name},</h1><h1>See yesterday's top voted posts from your favorite channel</h1>`;
   Object.keys(newsletter).forEach(channel => {
     newsletter[channel].forEach(post => {
@@ -8,16 +8,17 @@ export function buildHTML(user, newsletter) {
       if (post.content) {
         html += `<div class="img">${post.content}</div>`;
       } else if (post.preview) {
-        html += `<div class="img"><img style="max-width: 850px;" src="${post.preview}"></img></div>`;
+        html += `<div class="img"><img src="${post.preview}"></img></div>`;
       } else if (post.thumbnail) {
         html += `<div class="img"><img style="height: 30px;" src="${post.thumbnail}"></img></div>`;
       } else {
-        html += `<div class="img">NO IMAGE</div>`;
+        html += `<div class="img"><img style="width: 50%" src="https://i-love-png.com/images/no_image_png_935205.png"></img></div>`;
       }        
       html += `<div class="description"><div class="upvotes">${post.ups}</div><div class="title">${post.title}</div></div>`
       html += '</div>';
     });    
   })
   html += '</body></html>';
+  console.log(html);
   return html;
 }
